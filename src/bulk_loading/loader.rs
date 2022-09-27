7ecbec1548d2333b25ff4e96090fada4d4055f7c
Steven <steventhomson72@yahoo.ca>
2022-09-27 00:30:09 -0400

    Standardized copy and fixed bugs

--- src/bulk_loading/loader.rs
+++ src/bulk_loading/loader.rs
@@ -1,6 +1,6 @@
 use calamine::{open_workbook_auto, DataType, Reader};
 use geo_types::Geometry;
-use geojson::{FeatureReader};
+use geojson::FeatureReader;
 use itertools::Itertools;
 use polars::io::ipc::IpcReader;
 use polars::prelude::{AnyValue, DataFrame, ParquetReader, PolarsError, SerReader};
@@ -12,7 +12,7 @@ use sqlx::{PgPool, Postgres};
 use std::fmt::{Display, Write};
 use std::fs::File;
 use std::io::BufReader;
-use std::path::PathBuf;
+use std::path::Path;
 use tokio::fs::File as TkFile;
 use wkt::ToWkt;
 
@@ -126,15 +126,25 @@ impl CopyOptions {
             }
         )
     }
+
+    fn csv_copy_statement(&self) -> String {
+        format!(
+            "COPY {} ({}) FROM STDIN WITH (FORMAT csv, DELIMITER ',', HEADER false, NULL '', QUOTE '\"', ESCAPE '\"')",
+            self.table_name.to_lowercase(),
+            self.columns.join(","),
+        )
+    }
 }
 
-async fn copy_csv_row(
-    copy: &mut PgCopyIn<PoolConnection<Postgres>>,
-    mut csv_data: String,
-) -> Result<(), LoaderError> {
+pub type CopyPipe = PgCopyIn<PoolConnection<Postgres>>;
+pub type BulkLoadResult = Result<u64, LoaderError>;
+pub type CopyResult = Result<(), LoaderError>;
+
+async fn copy_csv_row(copy: &mut CopyPipe, mut csv_data: String) -> Result<(), LoaderError> {
     if csv_data.ends_with(',') {
         csv_data.pop();
     }
+    csv_data.push('\n');
     copy.send(csv_data.as_bytes()).await?;
     Ok(())
 }
@@ -170,14 +180,7 @@ fn append_formatted_value(buffer: &mut String, value: AnyValue) -> std::fmt::Res
     }
 }
 
-async fn load_dataframe(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    dataframe: DataFrame,
-) -> BulkLoadResult {
-    let copy_statement = copy_options.copy_statement(&',', &true, &true);
-    let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
-
+async fn load_dataframe(copy: &mut CopyPipe, dataframe: DataFrame) -> CopyResult {
     let mut iters = dataframe.iter().map(|s| s.iter()).collect::<Vec<_>>();
     for _ in 0..dataframe.height() {
         let mut csv_row = String::new();
@@ -187,30 +190,23 @@ async fn load_dataframe(
                 .ok_or("Dataframe value was not found. This should never happen".to_string())?;
             append_formatted_value(&mut csv_row, value)?;
         }
-        copy_csv_row(&mut copy, csv_row).await?;
+        copy_csv_row(copy, csv_row).await?;
     }
-    Ok(copy.finish().await?)
+    Ok(())
 }
 
-pub type BulkLoadResult = Result<u64, LoaderError>;
-
-async fn load_delimited_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-    delimiter: &char,
-    qualified: &bool,
-) -> BulkLoadResult {
-    let copy_statement = copy_options.copy_statement(delimiter, &true, qualified);
-    let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
-    let file = TkFile::open(file_path.as_path()).await?;
+async fn load_delimited_data(copy: &mut CopyPipe, file_path: &Path) -> CopyResult {
+    let file = TkFile::open(file_path).await?;
     copy.read_from(file).await?;
-    Ok(copy.finish().await?)
+    Ok(())
 }
 
 fn append_excel_value(buffer: &mut String, value: &DataType) -> Result<(), LoaderError> {
     match value {
-        DataType::String(s) => escape_csv_string_into_buffer(buffer, &s)?,
+        DataType::String(s) => escape_csv_string_into_buffer(
+            buffer,
+            &s.replace("_x000d_", "\n").replace("_x000a_", "\r"),
+        )?,
         DataType::DateTime(_) => {
             let formatted_datetime = value
                 .as_datetime()
@@ -218,25 +214,18 @@ fn append_excel_value(buffer: &mut String, value: &DataType) -> Result<(), Loade
                     "Cell error. Should be datetime but found something else. {}",
                     value
                 ))?
-                .format("%Y-%m-%d %H:%M:%S%.6f");
-            write!(buffer, "{}", formatted_datetime)?;
+                .format("%Y-%m-%d %H:%M:%S");
+            write!(buffer, "{},", formatted_datetime)?;
         }
         DataType::Error(e) => return Err(LoaderError::Generic(format!("Cell error, {}", e))),
         DataType::Empty => write!(buffer, ",")?,
-        _ => write!(buffer, "{}", value)?,
+        _ => write!(buffer, "{},", value)?,
     }
     Ok(())
 }
 
-async fn load_excel_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-    sheet_name: &String,
-) -> BulkLoadResult {
-    let copy_statement = copy_options.copy_statement(&',', &true, &true);
-    let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
-    let mut workbook = open_workbook_auto(file_path.as_path())?;
+async fn load_excel_data(copy: &mut CopyPipe, file_path: &Path, sheet_name: &String) -> CopyResult {
+    let mut workbook = open_workbook_auto(file_path)?;
     let sheet = match workbook.worksheet_range(&sheet_name) {
         Some(Ok(sheet)) => sheet,
         _ => {
@@ -246,14 +235,35 @@ async fn load_excel_data(
             )))
         }
     };
-    for row in sheet.rows() {
+    let mut rows = sheet.rows();
+    let header = match rows.next() {
+        Some(row) => row,
+        None => {
+            return Err(LoaderError::Generic(format!(
+                "Could not find a header row for excel file {:?}",
+                file_path
+            )))
+        }
+    };
+    let header_size = header.len();
+    let mut row_num = 0;
+    for row in rows {
+        row_num += 1;
+        let mut value_count = 0;
         let mut csv_row = String::new();
         for col in row {
             append_excel_value(&mut csv_row, col)?;
+            value_count += 1;
         }
-        copy_csv_row(&mut copy, csv_row).await?;
+        if value_count != header_size {
+            return Err(LoaderError::Generic(format!(
+                "Excel row {} has {} values but expected {}",
+                row_num, value_count, header_size
+            )));
+        }
+        copy_csv_row(copy, csv_row).await?;
     }
-    Ok(copy.finish().await?)
+    Ok(())
 }
 
 fn append_option_to_buffer<T: Display>(
@@ -297,14 +307,8 @@ fn append_field_value(buffer: &mut String, value: &FieldValue) -> std::fmt::Resu
     }
 }
 
-async fn load_shape_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-) -> BulkLoadResult {
-    let copy_statement = copy_options.copy_statement(&',', &true, &true);
-    let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
-    let mut reader = shapefile::Reader::from_path(file_path.as_path())?;
+async fn load_shape_data(copy: &mut CopyPipe, file_path: &Path) -> CopyResult {
+    let mut reader = shapefile::Reader::from_path(file_path)?;
     for feature in reader.iter_shapes_and_records() {
         let (shape, record) = feature?;
         let mut csv_row = String::new();
@@ -318,19 +322,13 @@ async fn load_shape_data(
                 write!(&mut csv_row, "{},", geo.wkt_string())?;
             }
         }
-        copy_csv_row(&mut copy, csv_row).await?;
+        copy_csv_row(copy, csv_row).await?;
     }
-    Ok(copy.finish().await?)
+    Ok(())
 }
 
-async fn load_geo_json_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-) -> BulkLoadResult {
-    let copy_statement = copy_options.copy_statement(&',', &true, &true);
-    let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
-    let file = File::open(file_path.as_path())?;
+async fn load_geo_json_data(copy: &mut CopyPipe, file_path: &Path) -> CopyResult {
+    let file = File::open(file_path)?;
     let buff_reader = BufReader::new(file);
     let feature_reader = FeatureReader::from_reader(buff_reader);
     for feature in feature_reader.features() {
@@ -346,73 +344,79 @@ async fn load_geo_json_data(
             .map(|(_, value)| value as &dyn Display)
             .chain(std::iter::once(&geom as &dyn Display))
             .join(",");
-        copy_csv_row(&mut copy, csv_row).await?;
+        copy_csv_row(copy, csv_row).await?;
     }
-    Ok(copy.finish().await?)
+    Ok(())
 }
 
-async fn load_parquet_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-) -> BulkLoadResult {
-    let file = File::open(file_path.as_path())?;
+async fn load_parquet_data(copy: &mut CopyPipe, file_path: &Path) -> CopyResult {
+    let file = File::open(file_path)?;
     let df = ParquetReader::new(file).finish()?;
-    load_dataframe(copy_options, pool, df).await
+    load_dataframe(copy, df).await
 }
 
-async fn load_ipc_data(
-    copy_options: &CopyOptions,
-    pool: &PgPool,
-    file_path: &PathBuf,
-) -> BulkLoadResult {
-    let file = File::open(file_path.as_path())?;
+async fn load_ipc_data(copy: &mut CopyPipe, file_path: &Path) -> CopyResult {
+    let file = File::open(file_path)?;
     let df = IpcReader::new(file).finish()?;
-    load_dataframe(copy_options, pool, df).await
+    load_dataframe(copy, df).await
 }
 
-pub enum BulkDataLoader {
+pub enum BulkDataLoader<'p> {
     DelimitedData {
-        file_path: PathBuf,
+        file_path: &'p Path,
         delimiter: char,
         qualified: bool,
     },
     Excel {
-        file_path: PathBuf,
+        file_path: &'p Path,
         sheet_name: String,
     },
     Shape {
-        file_path: PathBuf,
+        file_path: &'p Path,
     },
     GeoJSON {
-        file_path: PathBuf,
+        file_path: &'p Path,
     },
     Parquet {
-        file_path: PathBuf,
+        file_path: &'p Path,
     },
     Ipc {
-        file_path: PathBuf,
+        file_path: &'p Path,
     },
 }
 
-impl BulkDataLoader {
+impl<'p> BulkDataLoader<'p> {
     pub async fn load_data(&self, copy_options: CopyOptions, pool: PgPool) -> BulkLoadResult {
-        match self {
-            Self::DelimitedData {
-                file_path,
-                delimiter,
-                qualified,
-            } => load_delimited_data(&copy_options, &pool, file_path, delimiter, qualified).await,
+        let copy_statement = if let Self::DelimitedData {
+            delimiter,
+            qualified,
+            ..
+        } = self
+        {
+            copy_options.copy_statement(delimiter, &true, qualified)
+        } else {
+            copy_options.csv_copy_statement()
+        };
+        let mut copy = pool.copy_in_raw(copy_statement.as_str()).await?;
+        let result = match self {
+            Self::DelimitedData { file_path, .. } => {
+                load_delimited_data(&mut copy, file_path).await
+            }
             Self::Excel {
                 file_path,
                 sheet_name,
-            } => load_excel_data(&copy_options, &pool, file_path, sheet_name).await,
-            Self::Shape { file_path } => load_shape_data(&copy_options, &pool, file_path).await,
-            Self::GeoJSON { file_path } => {
-                load_geo_json_data(&copy_options, &pool, file_path).await
+            } => load_excel_data(&mut copy, file_path, sheet_name).await,
+            Self::Shape { file_path } => load_shape_data(&mut copy, file_path).await,
+            Self::GeoJSON { file_path } => load_geo_json_data(&mut copy, file_path).await,
+            Self::Parquet { file_path } => load_parquet_data(&mut copy, file_path).await,
+            Self::Ipc { file_path } => load_ipc_data(&mut copy, file_path).await,
+        };
+        match result {
+            Ok(_) => Ok(copy.finish().await?),
+            Err(error) => {
+                copy.abort(format!("{}", error)).await?;
+                Err(error)
             }
-            Self::Parquet { file_path } => load_parquet_data(&copy_options, &pool, file_path).await,
-            Self::Ipc { file_path } => load_ipc_data(&copy_options, &pool, file_path).await,
         }
     }
 }
