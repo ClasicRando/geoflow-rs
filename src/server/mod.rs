d5a71ef82d2d2d4e1c337c75583e9351814eee39
Steven Thomson <steventhomson9@gmail.com>
2022-12-18 13:40:57 -0500

    feat: add config to server build

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -6,7 +6,7 @@ use bulk_loading::{
     create_source_data, delete_source_data, read_many_source_data, read_single_source_data,
     update_source_data,
 };
-use rocket::{routes, Build, Rocket};
+use rocket::{routes, Build, Config, Rocket};
 use users::{
     add_user_role, create_user, login, logout, read_user, read_users, remove_user_role,
     update_user_name, update_user_password,
@@ -14,7 +14,11 @@ use users::{
 
 pub async fn build_server() -> Result<Rocket<Build>, sqlx::Error> {
     let pool = create_db_pool().await?;
-    Ok(rocket::build().manage(pool).mount(
+    let config = Config {
+        port: 8001,
+        ..Default::default()
+    };
+    Ok(rocket::build().manage(pool).configure(config).mount(
         "/api/v1/",
         routes![
             create_source_data,
